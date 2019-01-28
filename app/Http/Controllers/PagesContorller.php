f22fc45b060263d5631de39e0ed3e287e6283b50
Amina Salcin <asalcin2@etf.unsa.ba>
2019-01-28 21:34:44 +0100

    drugi komit

---
+++ app/Http/Controllers/PagesContorller.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PagesContorller extends Controller
+{
+    public function index(){
+        return 'aaa'; 
+    }
+}
